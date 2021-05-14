7d991eab082c567bd12d161ec166b70de1d531e0
jinyoung <john9803@likelion.org>
2021-05-15 03:25:58 +0900

    django course 9 commit

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
     #return HttpResponse('hello_world') # 웹페이지에서 보이는 컨텐츠를 정해준다!
