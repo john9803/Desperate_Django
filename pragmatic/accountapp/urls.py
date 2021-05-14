c5dd8c2d2fd67b6d3540ab3ceb3897df307645d0
jinyoung <john9803@likelion.org>
2021-05-15 03:00:27 +0900

    djust Django course 8 commit

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-
 from accountapp.views import hello_world
 
 app_name = "accountapp" # 브라우저에서 접근할때 ip가 아니라 app_name으로 들어올 수 있게 해준다!!
-
-
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return HttpResponse('hello_world') # 웹페이지에서 보이는 컨텐츠를 정해준다!
+    return render(request, 'base.html')
+    #return HttpResponse('hello_world') # 웹페이지에서 보이는 컨텐츠를 정해준다!
