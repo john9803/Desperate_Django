c349b5cac0d8367c897dda312a9672578d5c0ea0
jinyoung <john9803@likelion.org>
2021-05-18 20:37:39 +0900

    django course 21 commit

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
+
+# 경로지정
 
 app_name = "accountapp" # 브라우저에서 접근할때 ip가 아니라 app_name으로 들어올 수 있게 해준다!!
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create'), # class based view 일때 작성법!!
 
 ]
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -21,3 +24,12 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     #return HttpResponse('hello_world') # 웹페이지에서 보이는 컨텐츠를 정해준다!
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm # 기본적인 사용자 검증과정
+    success_url = reverse_lazy('accountapp:hello_world') #인증을 마치면 어리로 사용자를 돌려보낼 것인지를 알려주는코드
+    # 위의 Http에서의 reverse와 바로윗줄에 reverse_lazy는 무엇이 다른가? -> 함수와 클래스가 불러오는 방식의 차이로 클래스를 불러오려면 reverse_lazy를 사용함
+    template_name = 'accountapp/create.html'
+    # 회원가입을 할때 어떤 html 파일을 보여줄 것인지를 지정해야함.
+
