d1dec19118cf07ba84036084f33e80613ebcd941
jinyoung <john9803@likelion.org>
2021-05-19 18:10:50 +0900

    django course 32 commit

--- pragmatic/profileapp/urls.py
+++ pragmatic/profileapp/urls.py
@@ -1,9 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from profileapp.views import ProfileCreateView
+from profileapp.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns =[
     path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
