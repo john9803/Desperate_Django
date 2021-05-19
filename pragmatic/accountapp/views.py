45360f642655b5e12ae47d728e87a6272663b2ed
jinyoung <john9803@likelion.org>
2021-05-19 15:04:22 +0900

    Bug Fix

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -46,11 +46,13 @@ class UserUpdateForm(object):
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm # 기본적인 사용자 검증과정
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
