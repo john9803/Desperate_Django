9ac39a71d0e59244d168dfbcb77164ba1d5bd9f8
jinyoung <john9803@likelion.org>
2021-05-19 17:34:11 +0900

    django course 31 commit

--- pragmatic/profileapp/models.py
+++ pragmatic/profileapp/models.py
@@ -8,6 +8,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # User가 사라질때 Profile도 사라지는 기능
 
-    image = models.ImageField(upload_to='profile/', null=True)
+    image = models.ImageField(upload_to='profileapp/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
