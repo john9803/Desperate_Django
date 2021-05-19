e000d375d5ec7551ac77f5e2dbb258bf190a9be1
jinyoung <john9803@likelion.org>
2021-05-19 12:55:06 +0900

    django course 22 commit

--- pragmatic/accountapp/urls.py
+++ pragmatic/accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView
 
@@ -7,6 +8,9 @@ app_name = "accountapp" # 브라우저에서 접근할때 ip가 아니라 app_na
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'), # class based view 일때 작성법!!
 
