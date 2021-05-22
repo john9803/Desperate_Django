65d49f961a966f31e887b119f3665de8038eabd7
jinyoung <john9803@likelion.org>
2021-05-22 17:22:35 +0900

    django course 41 commit

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -6,11 +6,12 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
-
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 
@@ -44,10 +45,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
     # 회원가입을 할때 어떤 html 파일을 보여줄 것인지를 지정해야함.
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user' # 개인페이지의 유저를 바꿔주기위한 코드
     template_name = 'accountapp/detail.html'
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
 
 
 class UserUpdateForm(object):
